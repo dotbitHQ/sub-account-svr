d2858948dd16ca3f908a0d94ee97aac7c5d75f44
scorpiotzh <835598264@qq.com>
2022-06-28 16:19:59 +0800

    feat: update enable sub acc tx outputs data

--- http_server/handle/sub_account_init.go
+++ http_server/handle/sub_account_init.go
@@ -250,7 +250,7 @@ func (h *HttpHandle) buildSubAccountInitTx(p *paramsSubAccountInitTx) (*txbuilde
 		SmtRoot:          smt.H256Zero(),
 		DasProfit:        0,
 		OwnerProfit:      0,
-		CustomScriptArgs: nil,
+		CustomScriptArgs: make([]byte, 33),
 	}
 	subAccountOutputData := witness.BuildSubAccountCellOutputData(subDataDetail)
 	txParams.OutputsData = append(txParams.OutputsData, subAccountOutputData)
