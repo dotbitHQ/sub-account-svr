5c2c226d822e811fea75e42dc887109ae8b8ffef
scorpiotzh <835598264@qq.com>
2022-09-27 17:29:19 +0800

    feat: add slb svr

--- cmd/lb/main.go
+++ cmd/lb/main.go
@@ -2,15 +2,22 @@ package main
 
 import (
 	"context"
+	"das_sub_account/block_parser"
 	"das_sub_account/cache"
 	"das_sub_account/config"
+	"das_sub_account/dao"
 	"das_sub_account/http_server"
 	"das_sub_account/http_server/handle"
 	"das_sub_account/lb"
 	"fmt"
+	"github.com/dotbitHQ/das-lib/common"
+	"github.com/dotbitHQ/das-lib/core"
+	"github.com/nervosnetwork/ckb-sdk-go/rpc"
 	"github.com/scorpiotzh/mylog"
 	"github.com/scorpiotzh/toolib"
 	"github.com/urfave/cli/v2"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"os"
 	"sync"
 	"time"
@@ -54,6 +61,43 @@ func runServer(ctx *cli.Context) error {
 		return fmt.Errorf("AddCfgFileWatcher err: %s", err.Error())
 	}
 	// ============= service start =============
+
+	// das core
+	dasCore, err := initDasCore()
+	if err != nil {
+		return fmt.Errorf("initDasCore err: %s", err.Error())
+	}
+	log.Infof("das core ok")
+
+	// db
+	dbDao, err := dao.NewGormDB(config.Cfg.DB.Mysql, config.Cfg.DB.ParserMysql)
+	if err != nil {
+		return fmt.Errorf("NewGormDB err: %s", err.Error())
+	}
+	log.Infof("db ok")
+
+	// mongo
+	mongoClient, err := mongo.Connect(ctxServer, options.Client().ApplyURI(config.Cfg.DB.Mongo.Uri))
+	if err != nil {
+		return fmt.Errorf("mongo.Connect err:%s", err.Error())
+	}
+	log.Infof("mongo ok")
+
+	// block parser
+	blockParser := block_parser.BlockParser{
+		DasCore:            dasCore,
+		CurrentBlockNumber: config.Cfg.Chain.CurrentBlockNumber,
+		DbDao:              dbDao,
+		ConcurrencyNum:     config.Cfg.Chain.ConcurrencyNum,
+		ConfirmNum:         config.Cfg.Chain.ConfirmNum,
+		Mongo:              mongoClient,
+		Ctx:                ctxServer,
+		Wg:                 &wgServer,
+	}
+	if err := blockParser.Run(); err != nil {
+		return fmt.Errorf("blockParser.Run() err: %s", err.Error())
+	}
+	log.Infof("block parser ok")
 
 	// redis
 	red, err := toolib.NewRedisClient(config.Cfg.Cache.Redis.Addr, config.Cfg.Cache.Redis.Password, config.Cfg.Cache.Redis.DbNum)
@@ -102,4 +146,47 @@ func runServer(ctx *cli.Context) error {
 
 	<-exit
 	return nil
+}
+
+func initDasCore() (*core.DasCore, error) {
+	// ckb node
+	ckbClient, err := rpc.DialWithIndexer(config.Cfg.Chain.CkbUrl, config.Cfg.Chain.IndexUrl)
+	if err != nil {
+		return nil, fmt.Errorf("rpc.DialWithIndexer err: %s", err.Error())
+	}
+	log.Info("ckb node ok")
+
+	env := core.InitEnvOpt(config.Cfg.Server.Net,
+		common.DasContractNameConfigCellType,
+		common.DasContractNameAccountCellType,
+		common.DasContractNameBalanceCellType,
+		common.DasContractNameDispatchCellType,
+		common.DasContractNameAlwaysSuccess,
+		common.DASContractNameSubAccountCellType,
+		common.DASContractNameEip712LibCellType,
+	)
+
+	// das init
+	ops := []core.DasCoreOption{
+		core.WithClient(ckbClient),
+		core.WithDasContractArgs(env.ContractArgs),
+		core.WithDasContractCodeHash(env.ContractCodeHash),
+		core.WithDasNetType(config.Cfg.Server.Net),
+		core.WithTHQCodeHash(env.THQCodeHash),
+	}
+	dasCore := core.NewDasCore(ctxServer, &wgServer, ops...)
+	dasCore.InitDasContract(env.MapContract)
+	if err := dasCore.InitDasConfigCell(); err != nil {
+		return nil, fmt.Errorf("InitDasConfigCell err: %s", err.Error())
+	}
+	if err := dasCore.InitDasSoScript(); err != nil {
+		return nil, fmt.Errorf("InitDasSoScript err: %s", err.Error())
+	}
+	dasCore.RunAsyncDasContract(time.Minute * 3)   // contract outpoint
+	dasCore.RunAsyncDasConfigCell(time.Minute * 4) // config cell outpoint
+	dasCore.RunAsyncDasSoScript(time.Minute * 5)   // so
+
+	log.Info("das contract ok")
+
+	return dasCore, nil
 }
