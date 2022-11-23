42f5555548be65f47210546f121448c341f10f71
scorpiotzh <835598264@qq.com>
2022-11-23 15:21:39 +0800

    feat: add t_mint_sign_info

--- tables/t_smt_record_info.go
+++ tables/t_smt_record_info.go
@@ -33,7 +33,7 @@ type TableSmtRecordInfo struct {
 	EditRecords     string     `json:"edit_records" gorm:"column:edit_records;type:text NOT NULL COMMENT ''"`
 	Timestamp       int64      `json:"timestamp" gorm:"column:timestamp;type:bigint(20) NOT NULL DEFAULT '0' COMMENT 'record timestamp'"`
 	SubAction       string     `json:"sub_action" gorm:"column:sub_action; index:k_action; type:varchar(255) NOT NULL DEFAULT '' COMMENT '';"`
-	MintSignId      string     `json:"mint_sign_id" gorm:"column:mint_sign_id; index:uk_mint_sign_id; type:varchar(255) NOT NULL DEFAULT '' COMMENT '';"`
+	MintSignId      string     `json:"mint_sign_id" gorm:"column:mint_sign_id; index:k_mint_sign_id; type:varchar(255) NOT NULL DEFAULT '' COMMENT '';"`
 	CreatedAt       time.Time  `json:"created_at" gorm:"column:created_at;type:timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT ''"`
 	UpdatedAt       time.Time  `json:"updated_at" gorm:"column:updated_at;type:timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT ''"`
 }
