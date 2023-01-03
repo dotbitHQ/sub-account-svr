db847eeb1fe1f1e0c020ebfecbe9da38cc6c388b
scorpiotzh <835598264@qq.com>
2023-01-03 11:07:15 +0800

    feat: checkSystemUpgrade by contract

--- http_server/handle/handle.go
+++ http_server/handle/handle.go
@@ -57,6 +57,14 @@ func (h *HttpHandle) checkSystemUpgrade(apiResp *api_code.ApiResp) error {
 		apiResp.ApiRespErr(api_code.ApiCodeSystemUpgrade, api_code.TextSystemUpgrade)
 		return fmt.Errorf("contract system upgrade")
 	}
+	ok, err := h.DasCore.CheckContractStatusOK(common.DASContractNameSubAccountCellType)
+	if err != nil {
+		apiResp.ApiRespErr(api_code.ApiCodeError500, err.Error())
+		return fmt.Errorf("CheckContractStatusOK err: %s", err.Error())
+	} else if !ok {
+		apiResp.ApiRespErr(api_code.ApiCodeSystemUpgrade, api_code.TextSystemUpgrade)
+		return fmt.Errorf("contract system upgrade")
+	}
 	return nil
 }
 
