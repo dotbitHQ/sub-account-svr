e1ec14c29d8f283016b1320f81e6de00b564f649
Jerry <i@sjis.me>
2023-04-13 10:55:56 +0800

    feat: add router to slb server

--- http_server/lb_router.go
+++ http_server/lb_router.go
@@ -2,7 +2,9 @@ package http_server
 
 import (
 	"das_sub_account/config"
+	"das_sub_account/internal/static_files"
 	"github.com/scorpiotzh/toolib"
+	"net/http"
 )
 
 func (h *LbHttpServer) initRouter() {
@@ -25,11 +27,25 @@ func (h *LbHttpServer) initRouter() {
 		v1.POST("/custom/script/price", h.H.LBProxy)
 		v1.POST("/owner/profit", h.H.LBProxy)
 		v1.POST("/sub/account/check", h.H.LBProxy)
+		v1.POST("/statistical/info", h.H.LBProxy)
+		v1.POST("/distribution/list", h.H.LBProxy)
+		v1.POST("/currency/list", h.H.LBProxy)
+		v1.POST("/config/auto_mint/get", h.H.LBProxy)
+		v1.POST("/price/rule/list", h.H.LBProxy)
+		v1.POST("/preserved/rule/list", h.H.LBProxy)
+		v1.StaticFS("/static", http.FS(static_files.MintJs))
 
 		v1.POST("/sub/account/init", h.H.LBProxy) // enable_sub_account
 		v1.POST("/custom/script/set", h.H.LBProxy)
 		v1.POST("/profit/withdraw", h.H.LBProxy)
 		v1.POST("/sub/account/edit", h.H.LBProxy)
+		v1.POST("/mint/config/update", h.H.LBProxy)
+		v1.POST("/config/auto_mint/update", h.H.LBProxy)
+		v1.POST("/price/rule/update", h.H.LBProxy)
+		v1.POST("/preserved/rule/update", h.H.LBProxy)
+		v1.POST("/auto/account/search", h.H.LBProxy)
+		v1.POST("/auto/order/create", h.H.LBProxy)
+		v1.POST("/auto/order/hash", h.H.LBProxy)
 
 		v1.POST("/sub/account/create", h.H.LBSubAccountCreate) // create_sub_account
 		v1.POST("/transaction/send", h.H.LBTransactionSend)
