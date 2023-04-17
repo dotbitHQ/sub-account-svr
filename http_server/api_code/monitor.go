ee15a8e5814ec6771f8e851251ac15909311c1a3
scorpiotzh <835598264@qq.com>
2023-04-17 11:13:13 +0800

    feat: Cancel error code 40003 reporting

--- http_server/api_code/monitor.go
+++ http_server/api_code/monitor.go
@@ -53,6 +53,9 @@ func DoMonitorLog(method string) gin.HandlerFunc {
 				if resp.ErrNo != ApiCodeSuccess {
 					log.Warn("DoMonitorLog:", method, resp.ErrNo, resp.ErrMsg)
 				}
+				if resp.ErrNo == ApiCodeSignError {
+					resp.ErrNo = ApiCodeSuccess
+				}
 				pushLog := ReqPushLog{
 					Index:   config.Cfg.Server.PushLogIndex,
 					Method:  method,
