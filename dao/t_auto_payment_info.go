96affa13c925ce7f643202a0de79820e6a709eb1
Jerry <i@sjis.me>
2023-04-26 18:13:57 +0800

    feat: fix StatisticalInfo inaccurate data

--- dao/t_auto_payment_info.go
+++ dao/t_auto_payment_info.go
@@ -7,7 +7,7 @@ import (
 )
 
 func (d *DbDao) GetAutoPaymentAmount(accountId, tokenId string, paymentStatus tables.PaymentStatus) (amount decimal.Decimal, err error) {
-	err = d.db.Model(&tables.AutoPaymentInfo{}).Select("sum(amount)").Where("account_id=? and token_id=? and payment_status=?", accountId, tokenId, paymentStatus).Scan(&amount).Error
+	err = d.db.Model(&tables.AutoPaymentInfo{}).Select("IFNULL(sum(amount),0)").Where("account_id=? and token_id=? and payment_status=?", accountId, tokenId, paymentStatus).Scan(&amount).Error
 	if err == gorm.ErrRecordNotFound {
 		err = nil
 	}
