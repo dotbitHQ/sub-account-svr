d91b7ae396e0bcf1e7821ac961764faa11673532
Jerry <i@sjis.me>
2023-05-04 16:56:26 +0800

    feat: fix sql error

--- dao/t_sub_account_auto_mint_statement.go
+++ dao/t_sub_account_auto_mint_statement.go
@@ -6,7 +6,7 @@ import (
 )
 
 func (d *DbDao) FindSubAccountAutoMintTotalProvider() (list []string, err error) {
-	err = d.parserDb.Model(&tables.TableSubAccountAutoMintStatement{}).Select("service_provider_acc_id").Distinct("service_provider_acc_id").Group("service_provider_acc_id").Scan(&list).Error
+	err = d.parserDb.Model(&tables.TableSubAccountAutoMintStatement{}).Select("service_provider_id").Distinct("service_provider_id").Group("service_provider_id").Scan(&list).Error
 	if err == gorm.ErrRecordNotFound {
 		err = nil
 	}
@@ -22,7 +22,7 @@ func (d *DbDao) GetSubAccountAutoMintByTxHash(txHash string) (list tables.TableS
 }
 
 func (d *DbDao) GetLatestSubAccountAutoMintStatementByType(providerId string, txType tables.SubAccountAutoMintTxType) (list tables.TableSubAccountAutoMintStatement, err error) {
-	err = d.parserDb.Where("service_provider_acc_id=? and tx_type=?", providerId, txType).Order("id desc").First(&list).Error
+	err = d.parserDb.Where("service_provider_id=? and tx_type=?", providerId, txType).Order("id desc").First(&list).Error
 	if err == gorm.ErrRecordNotFound {
 		err = nil
 	}
@@ -30,7 +30,7 @@ func (d *DbDao) GetLatestSubAccountAutoMintStatementByType(providerId string, tx
 }
 
 func (d *DbDao) FindSubAccountAutoMintStatements(providerId string, txType tables.SubAccountAutoMintTxType, id uint64) (list []*tables.TableSubAccountAutoMintStatement, err error) {
-	err = d.parserDb.Where("id>? and service_provider_acc_id=? and tx_type=?", id, providerId, txType).Find(&list).Error
+	err = d.parserDb.Where("id>? and service_provider_id=? and tx_type=?", id, providerId, txType).Find(&list).Error
 	if err == gorm.ErrRecordNotFound {
 		err = nil
 	}
