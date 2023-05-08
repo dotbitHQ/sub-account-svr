83629320f577b5c00c44d658bf53f8224fe4dca6
jerry <i@sjis.me>
2023-05-08 21:38:41 +0800

    feat: update currency_update.go

--- http_server/handle/currency_update.go
+++ http_server/handle/currency_update.go
@@ -81,7 +81,7 @@ func (h *HttpHandle) doCurrencyUpdate(req *ReqCurrencyUpdate, apiResp *api_code.
 	if _, err = doSignCheck(txbuilder.SignData{
 		SignType: res.DasAlgorithmId,
 		SignMsg:  signMsg,
-	}, req.Signature, address, apiResp); err != nil {
+	}, req.Signature, req.KeyInfo.Key, apiResp); err != nil {
 		return fmt.Errorf("doSignCheck err: %s", err.Error())
 	} else if apiResp.ErrNo != api_code.ApiCodeSuccess {
 		return nil
