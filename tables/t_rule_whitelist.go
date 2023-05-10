b2a2c52eee7c809ac1cf7cc422a00be396c0b431
jerry <i@sjis.me>
2023-05-10 14:06:36 +0800

    feat: change t_rule_whitelist index

--- tables/t_rule_whitelist.go
+++ tables/t_rule_whitelist.go
@@ -14,11 +14,11 @@ type RuleWhitelist struct {
 	TxHash          string    `gorm:"index:idx_tx_hash;column:tx_hash;type:varchar(255);comment:交易hash;NOT NULL" json:"tx_hash"`
 	BlockNumber     int64     `gorm:"column:block_number;type:bigint(20);comment:区块高度;NOT NULL" json:"block_number"`
 	ParentAccount   string    `gorm:"column:parent_account;type:varchar(255);comment:父账号;NOT NULL" json:"parent_account"`
-	ParentAccountId string    `gorm:"index:idx_parent_acc_id;column:parent_account_id;type:varchar(255);comment:父账号id;NOT NULL" json:"parent_account_id"`
+	ParentAccountId string    `gorm:"index:idx_pid_acc_id;column:parent_account_id;type:varchar(255);comment:父账号id;NOT NULL" json:"parent_account_id"`
 	RuleType        RuleType  `gorm:"column:rule_type;type:varchar(255);comment:规则类型;NOT NULL" json:"rule_type"`
 	RuleIndex       int       `gorm:"column:rule_index;type:int(11);comment:规则索引;NOT NULL" json:"rule_index"`
 	Account         string    `gorm:"column:account;type:varchar(255);comment:账号;NOT NULL" json:"account"`
-	AccountId       string    `gorm:"index:idx_acc_id;column:account_id;type:varchar(255);comment:账号id;NOT NULL" json:"account_id"`
+	AccountId       string    `gorm:"index:idx_pid_acc_id;column:account_id;type:varchar(255);comment:账号id;NOT NULL" json:"account_id"`
 	TxStatus        TxStatus  `gorm:"column:tx_status;type:int(11);comment:交易状态;NOT NULL" json:"tx_status"`
 	BlockTimestamp  int64     `gorm:"column:block_timestamp;type:bigint(20);comment:区块时间戳;NOT NULL" json:"block_timestamp"`
 	CreatedAt       time.Time `gorm:"column:created_at;type:timestamp;default:CURRENT_TIMESTAMP;NOT NULL" json:"created_at"`
