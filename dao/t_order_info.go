54992a394c6983cbba4ffe9c54350bc3b0bd9da3
scorpiotzh <835598264@qq.com>
2023-05-22 18:25:34 +0800

    feat: check order status for func FindOrderByPayment

--- dao/t_order_info.go
+++ dao/t_order_info.go
@@ -16,7 +16,7 @@ func (d *DbDao) GetOrderByOrderID(orderID string) (order tables.OrderInfo, err e
 }
 
 func (d *DbDao) FindOrderByPayment(end int64, accountId string) (list []*tables.OrderInfo, err error) {
-	db := d.db.Model(&tables.OrderInfo{}).Where("auto_payment_id = '' AND pay_status=? AND timestamp<?", tables.PayStatusPaid, end)
+	db := d.db.Model(&tables.OrderInfo{}).Where("auto_payment_id = '' AND pay_status=? AND order_status=? AND timestamp<?", tables.PayStatusPaid, tables.OrderStatusSuccess, end)
 	if accountId != "" {
 		db = db.Where("parent_account_id=?", accountId)
 	}
