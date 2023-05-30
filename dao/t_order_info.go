bbcc370a3b1cbd182826647cd760c4e5eb5c4ba8
Jerry <i@sjis.me>
2023-05-30 17:34:09 +0800

    feat: sql where add order_status field

--- dao/t_order_info.go
+++ dao/t_order_info.go
@@ -124,7 +124,8 @@ type OrderAmountInfo struct {
 
 func (d *DbDao) GetOrderAmount(accountId string, paid bool) (result map[string]decimal.Decimal, err error) {
 	list := make([]*OrderAmountInfo, 0)
-	db := d.db.Model(&tables.OrderInfo{}).Select("token_id, sum(amount) as amount").Where("parent_account_id=? and pay_status=?", accountId, tables.PayStatusPaid)
+	db := d.db.Model(&tables.OrderInfo{}).Select("token_id, sum(amount) as amount").
+		Where("parent_account_id=? and pay_status=? and order_status=?", accountId, tables.PayStatusPaid, tables.OrderStatusSuccess)
 	if paid {
 		db = db.Where("auto_payment_id != ''")
 	}
