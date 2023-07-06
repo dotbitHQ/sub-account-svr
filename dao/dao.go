776ea017f6c318a4e0e0f4118a2089c15679a250
Jerry <i@sjis.me>
2023-07-06 12:51:07 +0800

    feat: delete TableRenewSignInfo

--- dao/dao.go
+++ dao/dao.go
@@ -39,7 +39,6 @@ func NewGormDB(dbMysql, parserMysql config.DbMysql, autoMigrate bool) (*DbDao, e
 			&tables.UserConfig{},
 			&tables.RuleWhitelist{},
 			&tables.TableSubAccountAutoMintWithdrawHistory{},
-			&tables.TableRenewSignInfo{},
 		); err != nil {
 			return nil, err
 		}
