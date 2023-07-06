03c79671ee16cc8027272507368a048b85227ee3
Jerry <i@sjis.me>
2023-07-06 12:50:08 +0800

    feat: delete GetRenewSignInfo

--- dao/t_renew_sign_info.go
+++
@@ -1,8 +0,0 @@
-package dao
-
-import "das_sub_account/tables"
-
-func (d *DbDao) GetRenewSignInfo(renewSignId string) (info tables.TableRenewSignInfo, err error) {
-	err = d.db.Where("renew_sign_id=?", renewSignId).First(&info).Error
-	return
-}
