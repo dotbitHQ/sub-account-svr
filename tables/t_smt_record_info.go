80e6064f7800632714113d890c8c98a1a14c27ce
michael <457813723@qq.com>
2023-07-25 11:44:59 +0800

    update signature field type

--- tables/t_smt_record_info.go
+++ tables/t_smt_record_info.go
@@ -31,7 +31,7 @@ type TableSmtRecordInfo struct {
 	RegisterYears   uint64           `json:"register_years" gorm:"column:register_years;type:int(11) NOT NULL DEFAULT '0' COMMENT ''"`
 	RegisterArgs    string           `json:"register_args" gorm:"column:register_args;type:varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '' COMMENT ''"`
 	EditKey         string           `json:"edit_key" gorm:"column:edit_key;type:varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '' COMMENT 'owner,manager,records'"`
-	Signature       string           `json:"signature" gorm:"column:signature;type:varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '' COMMENT ''"`
+	Signature       string           `json:"signature" gorm:"column:signature;type:text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '' COMMENT ''"`
 	LoginChainType  common.ChainType `json:"login_chain_type" gorm:"column:login_chain_type"`
 	LoginAddress    string           `json:"login_address" gorm:"column:login_address; type:varchar(255) NOT NULL DEFAULT '' COMMENT '';"`
 	SignAddress     string           `json:"sign_address" gorm:"column:sign_address; type:varchar(255) NOT NULL DEFAULT '' COMMENT '';"`
