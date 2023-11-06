e29372d0933ea3f9287351bb2b843372eb4f3880
Jerry <i@sjis.me>
2023-11-06 16:58:40 +0800

    feat: change coupon info index

--- tables/t_coupon_info.go
+++ tables/t_coupon_info.go
@@ -16,7 +16,7 @@ type CouponCode string
 type CouponInfo struct {
 	Id        uint64       `gorm:"column:id;primary_key;AUTO_INCREMENT;NOT NULL"`
 	Cid       string       `gorm:"index:idx_cid;column:cid;default:;NOT NULL"`
-	Code      string       `gorm:"index:idx_code;column:code;default:;NOT NULL"`
+	Code      string       `gorm:"uniqueIndex:idx_code;column:code;default:;NOT NULL"`
 	Status    CouponStatus `gorm:"column:status;default:0;NOT NULL"`
 	CreatedAt time.Time    `json:"created_at" gorm:"column:created_at; type:timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '';"`
 	UpdatedAt time.Time    `json:"updated_at" gorm:"column:updated_at; type:timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '';"`
