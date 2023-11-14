43989aebcc58d568a13e9b29acf88570b3c20f36
Jerry <i@sjis.me>
2023-11-14 22:33:51 +0800

    fix: Decrypt coupon code

--- dao/t_coupon_info.go
+++ dao/t_coupon_info.go
@@ -162,6 +162,13 @@ func (d *DbDao) GetCouponByCode(code string) (res tables.CouponInfo, err error) 
 		err = nil
 		return
 	}
+	if res.Id > 0 {
+		resCode, err := encrypt.AesDecrypt(res.Code, config.Cfg.Das.Coupon.EncryptionKey)
+		if err != nil {
+			return res, err
+		}
+		res.Code = resCode
+	}
 	return
 }
 
