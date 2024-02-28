7ebb81c9fea77f9e6ebdb7ee659d26a66c2cf1dd
scorpiotzh <835598264@qq.com>
2024-02-28 17:46:40 +0800

    feat: add api for padge

--- http_server/handle/padge_record_edit.go
+++ http_server/handle/padge_record_edit.go
@@ -1,7 +1,6 @@
 package handle
 
 import (
-	"das_sub_account/http_server/api_code"
 	"das_sub_account/tables"
 	"encoding/hex"
 	"fmt"
@@ -16,6 +15,10 @@ import (
 )
 
 type ReqPadgeRecordEdit struct {
+	List []ReqPadgeRecordEditData `json:"list"`
+}
+
+type ReqPadgeRecordEditData struct {
 	Account     string                   `json:"account"`
 	Nonce       uint64                   `json:"nonce"`
 	Signature   string                   `json:"signature"`
@@ -58,52 +61,58 @@ func (h *HttpHandle) PadgeRecordEdit(ctx *gin.Context) {
 func (h *HttpHandle) doPadgeRecordEdit(req *ReqPadgeRecordEdit, apiResp *http_api.ApiResp) error {
 	var resp RespPadgeRecordEdit
 
-	accountId := common.Bytes2Hex(common.GetAccountIdByAccount(req.Account))
-	acc, err := h.DbDao.GetAccountInfoByAccountId(accountId)
-	if err != nil {
-		apiResp.ApiRespErr(http_api.ApiCodeError500, err.Error())
-		return fmt.Errorf("GetAccountInfoByAccountId err: %s", err.Error())
-	} else if acc.Id == 0 {
-		apiResp.ApiRespErr(http_api.ApiCodeAccountNotExist, "account not exist")
-		return nil
-	} else if acc.ParentAccountId != "0x71cb663835a96d62020647e0bde504968558d5e6" {
-		apiResp.ApiRespErr(http_api.ApiCodeParamsInvalid, "account is invalid")
-		return nil
+	var list []tables.TableSmtRecordInfo
+	for _, v := range req.List {
+		accountId := common.Bytes2Hex(common.GetAccountIdByAccount(v.Account))
+		acc, err := h.DbDao.GetAccountInfoByAccountId(accountId)
+		if err != nil {
+			apiResp.ApiRespErr(http_api.ApiCodeError500, err.Error())
+			return fmt.Errorf("GetAccountInfoByAccountId err: %s", err.Error())
+		} else if acc.Id == 0 {
+			apiResp.ApiRespErr(http_api.ApiCodeAccountNotExist, "account not exist")
+			return nil
+		} else if acc.ParentAccountId != "0x71cb663835a96d62020647e0bde504968558d5e6" {
+			apiResp.ApiRespErr(http_api.ApiCodeParamsInvalid, "account is invalid")
+			return nil
+		}
+
+		payloadBys, _ := hex.DecodeString(v.Payload)
+		addressHex := common.FormatAddressPayload(payloadBys, v.AlgId)
+		addrNormal, err := h.DasCore.Daf().HexToNormal(core.DasAddressHex{
+			DasAlgorithmId:    v.AlgId,
+			DasSubAlgorithmId: v.SubAlgId,
+			AddressHex:        addressHex,
+			AddressPayload:    payloadBys,
+			IsMulti:           false,
+			ChainType:         v.AlgId.ToChainType(),
+		})
+
+		// add record
+		smtRecord := tables.TableSmtRecordInfo{
+			AccountId:       accountId,
+			Nonce:           v.Nonce,
+			RecordType:      tables.RecordTypeDefault,
+			Action:          common.DasActionUpdateSubAccount,
+			ParentAccountId: acc.ParentAccountId,
+			Account:         acc.Account,
+			EditKey:         "records",
+			Signature:       v.Signature,
+			LoginChainType:  v.AlgId.ToChainType(),
+			LoginAddress:    addrNormal.AddressNormal,
+			SignAddress:     v.SignAddress,
+			EditRecords:     toolib.JsonString(&v.Records),
+			Timestamp:       time.Now().UnixMilli(),
+			SubAction:       common.SubActionEdit,
+			ExpiredAt:       v.ExpiredAt,
+		}
+		list = append(list, smtRecord)
+
 	}
-
-	payloadBys, _ := hex.DecodeString(req.Payload)
-	addressHex := common.FormatAddressPayload(payloadBys, req.AlgId)
-	addrNormal, err := h.DasCore.Daf().HexToNormal(core.DasAddressHex{
-		DasAlgorithmId:    req.AlgId,
-		DasSubAlgorithmId: req.SubAlgId,
-		AddressHex:        addressHex,
-		AddressPayload:    payloadBys,
-		IsMulti:           false,
-		ChainType:         req.AlgId.ToChainType(),
-	})
-
-	// add record
-	smtRecord := tables.TableSmtRecordInfo{
-		AccountId:       accountId,
-		Nonce:           req.Nonce,
-		RecordType:      tables.RecordTypeDefault,
-		Action:          common.DasActionUpdateSubAccount,
-		ParentAccountId: acc.ParentAccountId,
-		Account:         acc.Account,
-		EditKey:         "records",
-		Signature:       req.Signature,
-		LoginChainType:  req.AlgId.ToChainType(),
-		LoginAddress:    addrNormal.AddressNormal,
-		SignAddress:     req.SignAddress,
-		EditRecords:     toolib.JsonString(&req.Records),
-		Timestamp:       time.Now().UnixMilli(),
-		SubAction:       common.SubActionEdit,
-		ExpiredAt:       req.ExpiredAt,
-	}
-
-	if err := h.DbDao.CreateSmtRecordInfo(smtRecord); err != nil {
-		apiResp.ApiRespErr(api_code.ApiCodeDbError, "fail to create smt record")
-		return fmt.Errorf("CreateSmtRecordInfo err:%s", err.Error())
+	if len(list) > 0 {
+		if err := h.DbDao.CreateSmtRecordList(list); err != nil {
+			apiResp.ApiRespErr(http_api.ApiCodeDbError, "fail to create smt record")
+			return fmt.Errorf("CreateSmtRecordList err:%s", err.Error())
+		}
 	}
 
 	apiResp.ApiRespOK(resp)
